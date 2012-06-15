450d5468cc780dc121dc95af91e5943ce4ca6a27
James Mac William <jmacwilliam@constantcontact.com>
2012-06-14 21:31:06 -0400

    generate project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransMarket::Application.config.secret_token = 'be1e38c025bb5b61e35e11ee508bddf6a93947f32953ea67800b6a696eaf4be399c9227cdf2056897be23e9b0ec00d730d5c0958a3c4ff0cf70f10d46e358309'
